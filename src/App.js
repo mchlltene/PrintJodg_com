4bbb15618e683f68d1da4a950e382afce01f85e2
EsterSangkoy <s21910288@student.unklab.ac.id>
2021-05-05 14:38:55 +0800

    tes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text>SEMANGAT QODIIIING | YAAAYY -MAESTROO</Text>
+      <Text>SEMANGAT QODIIIING aaaaaaaaaaaaaaa| YAAAYY -MAESTROO</Text>
     </View>
   );
 };
